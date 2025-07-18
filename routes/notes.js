b1e01e7d84ead78a95059b76ac523cdac780bfd4
Rico H <hrr600@gmail.com>
2025-07-18 15:01:22 -0400

    updating route authorizations

--- routes/notes.js
+++ routes/notes.js
@@ -20,11 +20,33 @@ router.get('/', async (req, res) => {
   }
 })
 
+// GET route /api/note/:id
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const note = await Note.findById(id)
+
+    if (!note) {
+      return res.status(404).json({ message: 'Not not found' })
+    }
+
+    if (!note.user.equals(req.user._id)) {
+      return res
+        .status(403)
+        .json({ message: 'User is not authorized to veiw note' })
+    }
+
+    res.json(note)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
 // POST /api/notes - Create a new note
 router.post('/', async (req, res) => {
   try {
     const note = await Note.create({
-      ...req.body,
+      ...req.body, // Spreading the request thru-out the body
       user: req.user._id // The user ID needs to be added here
     })
     res.status(201).json(note)
@@ -37,13 +59,23 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   try {
     // This needs an authorization check
-    const note = await Note.findByIdAndUpdate(req.params.id, req.body, {
-      new: true
-    })
+    const note = await Note.findById(req.params.id)
+
     if (!note) {
       return res.status(404).json({ message: 'No note found with this id!' })
     }
-    res.json(note)
+
+    if (!note.user.equals(req.user._id)) {
+      return res
+        .status(403)
+        .json({ message: 'Updates for this content by User are not permitted' })
+    }
+
+    const updatedNote = await Note.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    })
+
+    res.json(updatedNote)
   } catch (err) {
     res.status(500).json(err)
   }
@@ -57,6 +89,13 @@ router.delete('/:id', async (req, res) => {
     if (!note) {
       return res.status(404).json({ message: 'No note found with this id!' })
     }
+
+    if (!note.user.equals(req.user._id)) {
+      return res
+        .status(403)
+        .json({ message: 'User is not authorized to delete this content' })
+    }
+    await note.deleteOne()
     res.json({ message: 'Note deleted!' })
   } catch (err) {
     res.status(500).json(err)
